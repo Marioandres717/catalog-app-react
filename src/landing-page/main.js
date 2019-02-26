2e1858819739f614aa4af41e3cc4f6a0d0db98ab
Mario Andres <Marioarendon2@gmail.com>
2019-02-26 01:38:54 -0500

    fetch api

--- src/landing-page/main.js
+++ src/landing-page/main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import SimpleCard from './simpleCard';
+import Login from '../registration/login';
 
 const styles = (theme) => ({
   root: {
@@ -20,11 +21,27 @@ const styles = (theme) => ({
 });
 
 class Main extends Component {
+  state = {
+    categories: []
+  };
+
+  url = 'http://localhost:5000/';
+
+  async componentDidMount() {
+    try {
+      const response = await fetch(this.url);
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   render() {
     const { classes } = this.props;
-
     return (
       <div className={classes.root}>
+        <p> {console.log(this.categories)}</p>
         <Grid container spacing={24} justify="center">
           <Grid item xs={3}>
             <SimpleCard />
