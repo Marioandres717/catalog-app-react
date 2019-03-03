13a54b3a0c2b14fe38e196757b968d0bd2d9abcd
Mario Andres <Marioarendon2@gmail.com>
2019-03-02 21:00:46 -0500

    google login

--- src/components/registration/google.js
+++ src/components/registration/google.js
@@ -11,15 +11,41 @@ export default class Google extends Component {
   };
   responseGoogle = (response) => {
     console.log(response);
+    this.setState({
+      isLoggedIn: true,
+      userID: response.profileObj.googleId,
+      email: response.profileObj.email,
+      name: response.profileObj.givenName + response.profileObj.familyName,
+      picture: response.profileObj.imageUrl
+    });
   };
   render() {
-    return (
-      <GoogleLogin
-        clientId="143659131919-antcafffo6ca81dvvpikgjqrtfdfbvic.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={this.responseGoogle}
-        onFailure={this.responseGoogle}
-      />
-    );
+    let googleContent;
+
+    if (this.state.isLoggedIn) {
+      googleContent = (
+        <div
+          style={{
+            width: '400px',
+            margin: 'auto',
+            background: '#f4f4f4',
+            padding: '20px'
+          }}>
+          <img src={this.state.picture} alt={this.state.name} />
+          <h2>Welcome {this.state.name}</h2>
+          Email: {this.state.email}
+        </div>
+      );
+    } else {
+      googleContent = (
+        <GoogleLogin
+          clientId="143659131919-antcafffo6ca81dvvpikgjqrtfdfbvic.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+        />
+      );
+    }
+    return <div>{googleContent}</div>;
   }
 }
