f698e3247816882e5d0cc354c47bd0f90c951cd9
Mario Andres <Marioarendon2@gmail.com>
2019-03-19 11:16:18 -0400

    refactor login to use hooks

--- src/components/registration/login.js
+++ src/components/registration/login.js
@@ -1,50 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Facebook from './facebook';
 import Google from './google';
 import UserContext from '../../userContext';
 import Modal from '../../modal';
-export default class Login extends Component {
-  state = {
-    showModal: false
-  };
 
-  toggleModal = () => {
-    this.setState(state => ({
-      showModal: !state.showModal
-    }));
-  };
-
-  closeModal = () => {
-    this.setState({
-      showModal: false
-    });
-  };
-
-  render() {
-    var { showModal } = this.state;
-    // using function as a child pattern in order to get the context inside the component
-    return (
-      <UserContext.Consumer>
-        {user => (
-          <div>
-            {user[0].id != null ? (
-              <h1>Hello, {user[0].name}</h1>
-            ) : (
-              <button onClick={this.toggleModal}>Sign up | Login</button>
-            )}
-            {showModal ? (
-              <Modal>
-                <div>
-                  <h1>Join the Catalog community</h1>
-                  <Facebook closeModal={this.closeModal} />
-                  <Google closeModal={this.closeModal} />
-                  <button onClick={this.closeModal}>Close</button>
-                </div>
-              </Modal>
-            ) : null}
-          </div>
-        )}
-      </UserContext.Consumer>
-    );
+var Login = props => {
+  var [showModal, setModal] = useState(false);
+  console.log(showModal);
+  function toggleModal() {
+    setModal(!showModal);
   }
-}
+
+  function closeModal() {
+    setModal(false);
+  }
+
+  // using function as a child pattern in order to get the context inside the component
+  return (
+    <UserContext.Consumer>
+      {user => (
+        <div>
+          {user[0].id != null ? (
+            <h1>Hello, {user[0].name}</h1>
+          ) : (
+            <button onClick={toggleModal}>Sign up | Login</button>
+          )}
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Join the Catalog community</h1>
+                <Facebook closeModal={closeModal} />
+                <Google closeModal={closeModal} />
+                <button onClick={closeModal}>Close</button>
+              </div>
+            </Modal>
+          ) : null}
+        </div>
+      )}
+    </UserContext.Consumer>
+  );
+};
+
+export default Login;
