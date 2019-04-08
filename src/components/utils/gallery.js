359946dda6d236390e33370e75650df3beb9e400
Mario Andres <Marioarendon2@gmail.com>
2019-04-07 22:56:26 -0400

    fix import

--- src/components/utils/gallery.js
+++ src/components/utils/gallery.js
@@ -7,7 +7,7 @@ import {
   IconButton
 } from '@material-ui/core';
 import { navigate } from '@reach/router';
-import ItemForm from '../home/itemForm';
+import ItemForm from '../item/itemForm';
 import UserContext from '../../userContext';
 
 var styles = theme => ({
